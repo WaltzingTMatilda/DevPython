8f550f71d0910374c36dba0624cb40f448c9aaf4
WaltzingTMatilda <genbinsoku@gmail.com>
2020-01-13 16:57:18 +0900

    changing python_sample

--- python_sample.py
+++ python_sample.py
@@ -1,3 +1,15 @@
+#**************************************************#
+#文字列の重複カウント
+pattern = input()
+string = input()
+result = 0
+for i in range(len(string) - len(pattern) + 1):
+    portion = string[i:i + len(pattern)]
+    if portion == pattern:
+        result += 1
+print(result)
+#**************************************************#
+
 #**********************************************************#
 #結果サマリ
 key_num = int(input().rstrip())
