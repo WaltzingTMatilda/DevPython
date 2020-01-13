4e4cbd2adf9e462c2340ebf7b6d9a6db2cb75998
WaltzingTMatilda <54208357+WaltzingTMatilda@users.noreply.github.com>
2020-01-13 16:41:17 +0900

    Update new.py

--- new.py
+++ new.py
@@ -1,4 +1,25 @@
-日本の野鳥一覧
-犬の品種一覧
-猫の品種の一覧
-家畜一覧
+#**************************************************#
+#同単語をカウントし、表示
+input_words = []
+master_words = []
+input_words = input().rstrip().split(" ")
+master_words = list(dict.fromkeys(input_words))
+for i in master_words:
+    print(i + " " + str(input_words.count(i)))
+#**************************************************#
+
+#**************************************************#
+#文字列の重複カウント
+ans_count = 0
+key_words = input().rstrip()
+len_words= len(key_words)
+#read line&lenth
+line = input().rstrip()
+num_words = len(line)
+for iCnt in range(0,num_words):
+    if line[iCnt] == key_words[0]:
+        comp_words = line[iCnt:iCnt+len_words]
+        if comp_words == key_words:
+            ans_count += 1
+print(str(ans_count))
+#**************************************************#
